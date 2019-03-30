4ec3613493fe79c18d60c9ab34ab8e69a4f59c4d
unknown <devanshkejriwal@gmail.com>
2019-03-30 14:53:03 -0400

    added json file for alexa setup on developer console and basic code setup

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 const Alexa = require('ask-sdk-core');
 
 //skill name
-const appName = '';
+const appName = 'Investacito';
 
 //code for the handlers
 const LaunchRequestHandler = {
@@ -13,9 +13,13 @@ const LaunchRequestHandler = {
     },
     handle(handlerInput) {
         //welcome message
-        let speechText = '';
+        let speechText = `Welcome to Investacito. I will tell you how to make an
+        efficient portfolio AKA tangent portfolio with the return of your choice by
+        including risk free investments.You can say the return I want is 30%, My current
+        portfolio gives me a return of 20% and the risk free rate is 4% and I'll tell you the ratio
+        of your investments`;
         //welcome screen message
-        let displayText = ""
+        let displayText = "Welcome to Investacito"
         return handlerInput.responseBuilder
             .speak(speechText)
             .reprompt(speechText)
